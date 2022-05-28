59a494668009a3ad1a41235c5bd9901075ffa756
Ilnar Ihsanov <iixsanov@inbox.ru>
2022-05-28 23:53:14 +0300

    исправлены мелкие недочеты, сделан степпер

--- js/main.js
+++ js/main.js
@@ -16,10 +16,81 @@ function toggleModalDel() {
 function showAllFlower() {
   var classes = document.querySelectorAll('#hidden-item');
   console.log(classes);
-  for (let i = 0; i < 3; i++)
+  for (let i = 0; i < classes.length; i++)
   {
     classes[i].style.display = 'block';
   }
   document.getElementById('allFlowerText').style.display = 'none';
   document.getElementById('allFlowerArrow').style.display = 'none';
 }
+
+
+const stepper = document.querySelector('.products-item');
+const stepperInput = stepper.querySelector('.set-count__count-text');
+const stepperBtnUp = stepper.querySelector('.set-count__symbol-up');
+const stepperBtnDown = stepper.querySelector('.set-count__symbol-down');
+
+let count = stepperInput.value;
+
+stepperInput.addEventListener('keyup', (e) => {
+  let self = e.currentTarget;
+
+  if(self.value == '0') {
+    self.value = '1';
+  }
+
+  count = stepper.value;
+
+  if(count == 1) {
+    stepperBtnDown.classList.add('stepper-btn__disabled');
+  } else
+  stepperBtnDown.classList.remove('stepper-btn__disabled');
+});
+
+stepperInput.addEventListener('keydown', (e) => {
+  if(e.charCode && e.charCode != 0 && e.charCode != 46 && (e.charCode < 48 || e.charCode > 57))
+  {
+    return false;
+  }
+});
+
+stepperInput.addEventListener('change', (e) => {
+  let self = e.currentTarget;
+
+  if(!self.value)
+  {
+    self.value = 1;
+  }
+
+  count = stepperInput.value;
+
+  if(count == 1) {
+    stepperBtnDown.classList.add('stepper-btn__disabled');
+  } else
+  stepperBtnDown.classList.remove('stepper-btn__disabled');
+});
+
+stepperBtnUp.addEventListener('click', () => {
+
+  count++;
+
+  if(count == 1) {
+    stepperBtnDown.classList.add('stepper-btn__disabled');
+  } else
+  stepperBtnDown.classList.remove('stepper-btn__disabled');
+
+  stepperInput.value = count;
+});
+
+stepperBtnDown.addEventListener('click', () => {
+
+
+  count--;
+
+  if(count == 1) {
+    stepperBtnDown.classList.add('stepper-btn__disabled');
+  } else
+  stepperBtnDown.classList.remove('stepper-btn__disabled');
+
+  stepperInput.value = count;
+});
